990b3b545b0a9541f415d1115f52d69bcd4bff2f
Ravi Patel <ravifromchanga@gmail.com>
2025-04-19 13:52:10 +0530

    build error solved

--- src/app/drive/page.js
+++ src/app/drive/page.js
@@ -3,20 +3,35 @@
 import { useSearchParams } from "next/navigation";
 import DriveContent from "../components/DriveContent";
 import { Section } from "../components/ui";
+import { Suspense } from "react";
 
-export default function DrivePage() {
+function DrivePageContent() {
   const searchParams = useSearchParams();
   const folderId = searchParams.get("folderId");
   const heading = searchParams.get("heading");
 
   return (
+    <Section background="white" size="large">
+      <DriveContent
+        title={heading || "Drive Content"}
+        defaultFolderId={folderId || "1CrY0oeDZrnVZRuBwtTIp3MVIs9EoXCl2"}
+      />
+    </Section>
+  );
+}
+
+export default function DrivePage() {
+  return (
     <main>
-      <Section background="white" size="large">
-        <DriveContent
-          title={heading || "Drive Content"}
-          defaultFolderId={folderId || "1CrY0oeDZrnVZRuBwtTIp3MVIs9EoXCl2"}
-        />
-      </Section>
+      <Suspense
+        fallback={
+          <Section background="white" size="large">
+            Loading...
+          </Section>
+        }
+      >
+        <DrivePageContent />
+      </Suspense>
     </main>
   );
 }
